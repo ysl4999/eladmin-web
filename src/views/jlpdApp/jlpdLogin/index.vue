a3a056d493588f949063bb22ed3e823559400817
ysl4999 <forgetmenot1@163.com>
2023-05-13 14:02:55 +0800

    first commit

---
+++ src/views/jlpdApp/jlpdLogin/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="app-container">
+    <!--工具栏-->
+    <div class="head-container">
+      <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
+      <crudOperation :permission="permission" />
+      <!--表单组件-->
+      <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
+        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button type="text" @click="crud.cancelCU">取消</el-button>
+          <el-button :loading="crud.status.cu === 2" type="primary" @click="crud.submitCU">确认</el-button>
+        </div>
+      </el-dialog>
+      <!--表格渲染-->
+      <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
+        <el-table-column type="selection" width="55" />
+        <el-table-column prop="status" label="状态（0正常 1删除 2停用）" />
+        <el-table-column v-if="checkPer(['admin','testData:edit','testData:del'])" label="操作" width="150px" align="center">
+          <template slot-scope="scope">
+            <udOperation
+              :data="scope.row"
+              :permission="permission"
+            />
+          </template>
+        </el-table-column>
+      </el-table>
+      <!--分页组件-->
+      <pagination />
+    </div>
+  </div>
+</template>
+
+<script>
+import crudTestData from '@/api/jlpdApp/jlpdLogin'
+import CRUD, { presenter, header, form, crud } from '@crud/crud'
+import rrOperation from '@crud/RR.operation'
+import crudOperation from '@crud/CRUD.operation'
+import udOperation from '@crud/UD.operation'
+import pagination from '@crud/Pagination'
+
+const defaultForm = { id: null, testUserCode: null, testOfficeCode: null, testAreaCode: null, testAreaName: null, status: null, createBy: null, createDate: null, updateBy: null, updateDate: null, remarks: null }
+export default {
+  name: 'TestData',
+  components: { pagination, crudOperation, rrOperation, udOperation },
+  mixins: [presenter(), header(), form(defaultForm), crud()],
+  cruds() {
+    return CRUD({ title: '极乐派对登录', url: 'api/testData', idField: 'id', sort: 'id,desc', crudMethod: { ...crudTestData }})
+  },
+  data() {
+    return {
+      permission: {
+        add: ['admin', 'testData:add'],
+        edit: ['admin', 'testData:edit'],
+        del: ['admin', 'testData:del']
+      },
+      rules: {
+      }    }
+  },
+  methods: {
+    // 钩子：在获取表格数据之前执行，false 则代表不获取数据
+    [CRUD.HOOK.beforeRefresh]() {
+      return true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
